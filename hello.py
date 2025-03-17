22b3c43ec9edf6c85b3778dfff15932af4b23d4c
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-03-17 16:34:21 +0000

    mario10

--- hello.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-print("こんにちはpython!")
-
-message='Hello, Python!'
-print(message)
-
-message='Hello, World!'
-print(message)
---
+++ mario10/hello.py
@@ -0,0 +1,38 @@
+import pygame
+
+# 画面サイズを定義
+W, H = 320, 270
+
+# タイル数
+TILE_X = 16
+TILE_Y = 14
+
+def main():
+    ''' メイン関数 '''
+    # pygame初期化
+    pygame.init()
+    
+    # 画面を作成
+    win = pygame.display.set_mode((W, H))
+    
+    # クロックを作成
+    clock = pygame.time.Clock()
+
+    # イベントループ
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                return  # ループを抜ける
+
+        # 背景を塗りつぶす
+        win.fill((135, 206, 235))
+
+        # 画面全体を更新
+        pygame.display.flip()
+
+        # フレームレートを設定
+        clock.tick(30)
+
+if __name__ == '__main__':  # 修正
+    main()
